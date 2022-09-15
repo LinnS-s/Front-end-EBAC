d5bf4ef70206b8931eff2aaadf3bae0d65965975
Linns <joao.vflins@gmail.com>
2022-09-15 12:36:24 -0300

    modal:Concluido, projeto finalizado aguardando revisão

--- BootStrap/Projeto/custum.js
+++ BootStrap/Projeto/custum.js
@@ -1,9 +1,29 @@
-function abrir(){
-    const modal = document.getElementById("modal")
-    modal.classList.add("abrir")
+function abrir1(){
+    const modal1 = document.getElementById("modal1")
+    modal1.classList.add("abrir")
 
-    modal.addEventListener("click",(e)=>{
-        if(e.target.id=="fechar" || e.target.id == "modal")
-        modal.classList.remove("abrir")
+    modal1.addEventListener("click",(e)=>{
+        if(e.target.id=="fechar" || e.target.id == "modal1")
+        modal1.classList.remove("abrir")
+    })
+}
+
+function abrir2(){
+    const modal2 = document.getElementById("modal2")
+    modal2.classList.add("abrir")
+
+    modal2.addEventListener("click",(e)=>{
+        if(e.target.id=="fechar" || e.target.id == "modal2")
+        modal2.classList.remove("abrir")
+    })
+}
+
+function abrir3(){
+    const modal3 = document.getElementById("modal3")
+    modal3.classList.add("abrir")
+
+    modal3.addEventListener("click",(e)=>{
+        if(e.target.id=="fechar" || e.target.id === "modal3")
+        modal3.classList.remove("abrir")
     })
 }
